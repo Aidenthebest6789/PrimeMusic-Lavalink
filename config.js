96e07409fa3384e21234dd936a4775b9ddb3a1fc
Aidenthebest6789 <Sawyeraiden4@gmail.com>
2025-03-09 19:28:10 -0400

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ module.exports = {
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
+  activityName: "Support discord.gg/qAE9FaPTwu", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/qAE9FaPTwu",
   embedTimeout: 5, 
