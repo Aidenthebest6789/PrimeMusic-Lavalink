7f32ab923eaaaa3e4546877be56e912e21b2d61c
Aidenthebest6789 <Sawyeraiden4@gmail.com>
2025-03-09 19:47:02 -0400

    Update deleteplaylist.js

--- commands/deleteplaylist.js
+++ commands/deleteplaylist.js
@@ -18,7 +18,7 @@ async function deletePlaylist(client, interaction, lang) {
                     url: config.SupportServer
                 })
                 .setDescription(lang.deleteplaylist.embed.playlistNotFoundDescription)
-                .setFooter({ text: lang.footer, iconURL: musicIcons.heartIcon })
+                .setFooter({ text: "HexaBot Music", iconURL: musicIcons.heartIcon })
                 .setTimestamp();
 
             await interaction.reply({ embeds: [embed], ephemeral: true });
